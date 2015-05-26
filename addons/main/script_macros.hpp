259bdaf64fcf3b969ae451cc82e4f7aac093ea41
Niko Lehtovirta <niko.lehtovirta@hotmail.com>
2015-05-26 23:37:19 +0300

    Added main to include mod info for all other KGE modules

---
+++ addons/main/script_macros.hpp
@@ -0,0 +1,95 @@
+#include "\x\cba\addons\main\script_macros_common.hpp"
+#include "\x\cba\addons\xeh\script_xeh.hpp"
+
+// Default versioning level
+#define DEFAULT_VERSIONING_LEVEL 2
+
+// RGB Colors
+#define RGB_GREEN 0, 0.5, 0, 1
+#define RGB_BLUE 0, 0, 1, 1
+#define RGB_ORANGE 0.5, 0.5, 0, 1
+#define RGB_RED 1, 0, 0, 1
+#define RGB_YELLOW 1, 1, 0, 1
+#define RGB_WHITE 1, 1, 1, 1
+#define RGB_GRAY 0.5, 0.5, 0.5, 1
+#define RGB_BLACK 0, 0, 0, 1
+#define RGB_MAROON 0.5, 0, 0, 1
+#define RGB_OLIVE 0.5, 0.5, 0, 1
+#define RGB_NAVY 0, 0, 0.5, 1
+#define RGB_PURPLE 0.5, 0, 0.5, 1
+#define RGB_FUCHSIA 1, 0, 1, 1
+#define RGB_AQUA 0, 1, 1, 1
+#define RGB_TEAL 0, 0.5, 0.5, 1
+#define RGB_LIME 0, 1, 0, 1
+#define RGB_SILVER 0.75, 0.75, 0.75, 1
+
+
+#define EGVAR(module,var) TRIPLES(PREFIX,module,var)
+#define QEGVAR(module,var) QUOTE(EGVAR(module,var))
+
+#define DFUNC(var1) TRIPLES(ADDON,fnc,var1)
+#define DEFUNC(var1,var2) TRIPLES(DOUBLES(PREFIX,var1),fnc,var2)
+#define FUNC(var1) TRIPLES(ADDON,fnc,var1)
+#define EFUNC(var1,var2) TRIPLES(DOUBLES(PREFIX,var1),fnc,var2)
+
+#define QFUNC(var1) QUOTE(DFUNC(var1))
+#define QEFUNC(var1,var2) QUOTE(DEFUNC(var1,var2))
+
+#define PATHTOEF(var1,var2) PATHTOF_SYS(PREFIX,var1,var2)
+
+
+#define GETVAR_SYS(var1,var2) getVariable [ARR_2(QUOTE(var1),var2)]
+#define SETVAR_SYS(var1,var2) setVariable [ARR_2(QUOTE(var1),var2)]
+#define SETPVAR_SYS(var1,var2) setVariable [ARR_3(QUOTE(var1),var2,true)]
+
+#define GETVAR(var1,var2,var3) var1 GETVAR_SYS(var2,var3)
+#define GETMVAR(var1,var2) missionNamespace GETVAR_SYS(var1,var2)
+#define GETUVAR(var1,var2) uiNamespace GETVAR_SYS(var1,var2)
+#define GETPRVAR(var1,var2) profileNamespace GETVAR_SYS(var1,var2)
+#define GETPAVAR(var1,var2) parsingNamespace GETVAR_SYS(var1,var2)
+
+#define SETVAR(var1,var2,var3) var1 SETVAR_SYS(var2,var3)
+#define SETPVAR(var1,var2,var3) var1 SETPVAR_SYS(var2,var3)
+#define SETMVAR(var1,var2) missionNamespace SETVAR_SYS(var1,var2)
+#define SETUVAR(var1,var2) uiNamespace SETVAR_SYS(var1,var2)
+#define SETPRVAR(var1,var2) profileNamespace SETVAR_SYS(var1,var2)
+#define SETPAVAR(var1,var2) parsingNamespace SETVAR_SYS(var1,var2)
+
+#define GETGVAR(var1,var2) GETMVAR(GVAR(var1),var2)
+#define GETEGVAR(var1,var2,var3) GETMVAR(EGVAR(var1,var2),var3)
+
+#define ARR_SELECT(ARRAY,INDEX,DEFAULT) if (count ARRAY > INDEX) then {ARRAY select INDEX} else {DEFAULT}
+
+#ifdef DISABLE_COMPILE_CACHE
+    #define PREP(fncName) DFUNC(fncName) = compile preprocessFileLineNumbers QUOTE(PATHTOF(functions\DOUBLES(fnc,fncName).sqf))
+#else
+    #define PREP(fncName) DFUNC(fncName) = QUOTE(PATHTOF(functions\DOUBLES(fnc,fncName).sqf)) call SLX_XEH_COMPILE
+#endif
+
+#define PREP_MODULE(folder) [] call compile preprocessFileLineNumbers QUOTE(PATHTOF(folder\__PREP__.sqf))
+
+#define FUNC(var1) TRIPLES(ADDON,fnc,var1)
+#define EFUNC(var1,var2) TRIPLES(DOUBLES(PREFIX,var1),fnc,var2)
+
+#define LOCALIZATION(var1) localize QUOTE(DOUBLES(STR,GVAR(var1)))
+
+#define HASMOD(var1) [ARR_1(QUOTE(var1))] call KGE_Core_fnc_classExist
+
+// Supported mod defines
+#define MOD_ACE_COMMON HASMOD(ace_main)
+#define MOD_ACE_MENU HASMOD(ace_interact_menu)
+#define MOD_TFAR HASMOD(task_force_radio)
+#define MOD_ACRE2 HASMOD(acre_main)
+
+// KGE mods
+#define KGE_MOD_AI 		   HASMOD(kge_ai)
+#define KGE_MOD_CACHE 	   HASMOD(kge_cache)
+#define KGE_MOD_GEAR 	   HASMOD(kge_gear)
+#define KGE_MOD_MENU 	   HASMOD(kge_menu)
+#define KGE_MOD_RESPAWN    HASMOD(kge_respawn)
+#define KGE_MOD_SAFESTART  HASMOD(kge_safestart)
+#define KGE_MOD_SPECTATOR  HASMOD(kge_spectator)
+#define KGE_MOD_STAMINABAR HASMOD(kge_staminabar)
+#define KGE_MOD_TEAMROSTER HASMOD(kge_teamroster)
+#define KGE_MOD_TELEPORT   HASMOD(kge_teleport)
+#define KGE_MOD_ZEUS       HASMOD(kge_zeus)
---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,15 @@
+#define MAINPREFIX z
+#define PREFIX kge
+
+#define MAJOR 1
+#define MINOR 0
+#define PATCHLVL 0
+#define BUILD 0
+
+#define VERSION MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
+
+#define KGE_TAG K.G.E
+
+// MINIMAL required version for the Mod. Components can specify others..
+#define REQUIRED_VERSION 0.5
