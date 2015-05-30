8f2e5fc8733cf5cf34ada3e07bbd4ae743dc024f
Niko Lehtovirta <niko.lehtovirta@hotmail.com>
2015-05-30 14:43:19 +0300

    removed old KGE code from addons

--- addons/f_spect/config_eventhandlers.hpp
+++
@@ -1,7 +0,0 @@
-#include "script_component.hpp"
-
-class Extended_PreInit_EventHandlers {
-  class ADDON {
-    init = QUOTE( call COMPILE_FILE(XEH_PreInit) );
-  };
-};
--- addons/menu/functions/fn_EmptyMenu.sqf
+++
@@ -1,3 +0,0 @@
-lbclear 5133;
-ctrlShow [5133, false];
-ctrlShow [5134, false];
