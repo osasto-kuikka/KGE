9e47ad5d3568f574532478548928971970bd3848
nikolauska <nikolauska1@gmail.com>
2015-06-04 12:54:14 +0300

    renamed better_chat to clearchat

--- addons/clearchat/config.cpp
+++ addons/clearchat/config.cpp
@@ -18,12 +18,12 @@ class RscDisplayChat {
         delete Line;
         delete Background;
         class CA_Background: RscText {
-            colorBackground[] = {0.5,0.5,0.5,0.33}; // Make the chat entry field slightly darker
+            colorBackground[] = {0.5,0.5,0.5,0.33}; // chat entry field
         };
     };
 };
 
 class RscChatListDefault {
-    colorBackground[] = {0,0,0,0.5}; // Make the chat background darker
-    colorMessageProtocol[] = {0.85,0.85,0.85,1}; // And the chat text brighter
+    colorBackground[] = {0,0,0,0.5}; // chat background
+    colorMessageProtocol[] = {0.85,0.85,0.85,1}; // chat text
 };
