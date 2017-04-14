9969f31b5225f87b3a701a89c3a86bc554f4d18d
nikolauska <nikolauska1@gmail.com>
2017-04-14 22:15:27 +0300

    Remove disableModuload and fix version quote

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -6,7 +6,7 @@
 #define PATCHLVL 7
 #define BUILD 0
 
-#define VERSION MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION QUOTE(MAJOR.MINOR.PATCHLVL.BUILD)
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
